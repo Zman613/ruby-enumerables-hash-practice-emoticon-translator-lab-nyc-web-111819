96777f521539dfa70c64f4dedc4ce4187459981e
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-15 06:45:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -31,7 +31,7 @@ end
 def get_english_meaning(path, emoticon)
   # code goes here
   load = load_library(path)
-    if load["get_meaning"][emoticon]
+    if load.any? (emoticon)
       return load["get_meaning"][emoticon]
     else
       puts "Sorry, that emoticon was not found"
