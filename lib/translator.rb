ea0b6dff5ad453011d9e24fb409720e746fb9c83
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-15 05:52:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,7 @@ def load_library(path)
   # code goes here
   library = {}
   library = YAML.load_file(path)
-  library << {"get_meaning" => {}}
-  library << {"get_emoticon" => {}}
+
 end
 
 def get_japanese_emoticon
