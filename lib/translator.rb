e4aa51b4eb7ab100397fa619b1fac59135b3c76c
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-15 06:49:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -33,6 +33,7 @@ def get_english_meaning(path, emoticon)
   load = load_library(path)
     if load["get_meaning"][emoticon]
       return load["get_meaning"][emoticon]
-    end
+    else
       puts "Sorry, that emoticon was not found"
+    end
 end
